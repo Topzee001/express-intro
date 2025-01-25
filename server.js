623b52951afe0b04aeb8b0672473caf591477d37
Ibrahim <ibrahimsakariyah@gmail.com>
2025-01-25 22:21:55 +0100

    how to connect hosted db and local db to my app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
@@ -5,10 +6,28 @@ dotenv.config({ path: './config.env' });
 
 // console.log(app.get('env'));
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
+mongoose
+
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    // console.log(con.connections);
+    console.log('DB connection successful'),
+  );
 // Start Server
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//to connect to local database
+//   // .connect(process.env.DATABASE_LOCAL, {
